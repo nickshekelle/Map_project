557de4293a938f51c6125879ba70c1f582af1a64
Nick Shekelle <nshekelle@varmour.com>
2019-07-30 18:57:40 -0700

    fixed resizing

--- src/map_component.js
+++ src/map_component.js
@@ -1,29 +1,59 @@
-import React, { useState, useEffect } from "react";
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
+
+import React, { Component } from 'react';
+import MapGL from 'react-map-gl';
 
 
-export default function Map(){
-  const [viewport, setViewport] = useState({
-    latitude: 45.4211,
-    longitude: -75.6903,
-    width: "100vw",
-    height: "100vh",
-    zoom: 10
-  });
+export default class Map extends Component {
+  state = {
+    style: 'mapbox://styles/nickshekelle/cjyqkb5bz28mq1cof9u8yuk6e',
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      longitude: -122.0376692,
+      latitude: 36.9734858,
+      zoom: 11,
+      maxZoom: 16
+    }
+  }
 
-  
+  componentDidMount() {
+    window.addEventListener('resize', this._resize);
+    this._resize();
+  }
 
-  return (
-    <div>
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken='pk.eyJ1Ijoibmlja3NoZWtlbGxlIiwiYSI6ImNqeW90aWhrODE4Y20zbXA3eGNkZ2JsankifQ.ge-O4WjAvMnMBBo8rBpScw'
-        mapStyle="mapbox://styles/nickshekelle/cjyqkb5bz28mq1cof9u8yuk6e"
-        onViewportChange={viewport => {
-          setViewport(viewport);
-        }}>
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._resize);
+  }
 
-        </ReactMapGL>
-        </div>
-  );  
-}      
+  onStyleChange = (style) => {
+    this.setState({style});
+  }
+
+  _onViewportChange = (viewport) => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    });
+  }
+
+  _resize = () => {
+    this._onViewportChange({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <MapGL
+          mapboxApiAccessToken='pk.eyJ1Ijoibmlja3NoZWtlbGxlIiwiYSI6ImNqeW90aWhrODE4Y20zbXA3eGNkZ2JsankifQ.ge-O4WjAvMnMBBo8rBpScw'
+          {...this.state.viewport}
+          mapStyle={this.state.style}
+          onViewportChange={viewport => this._onViewportChange(viewport)}
+        >
+        </MapGL>
+      </div>
+    );
+  }
+}
+
