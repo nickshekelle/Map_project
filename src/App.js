c2246459126bb17cb40f08ac0c708bd822bbb92e
Nick Shekelle <nshekelle@varmour.com>
2019-08-08 10:39:20 -0700

    condition rendering progress

--- src/App.js
+++ src/App.js
@@ -15,40 +15,79 @@ import Button from 'react-bootstrap/Button'
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.whichmap = "map1";
+    this.state = { mode: 'view' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
   }
 
-  function(evt) {
-    this.whichmap = evt
-    console.log(this.whichmap)
+  handleChange(e) {
+    this.setState({ inputText: e.target.value });
+  }
+
+  handleSave() {
+    this.setState({ mode: 'view' });
+  }
+
+  handleEdit() {
+    this.setState({ mode: 'edit' });
   }
 
 
   render() {
-    return (
-      <div>
-        <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
-          <Navbar.Brand href="#home">Flood Zone</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Resources" id="basic-nav-dropdown" onSelect={function (evt) { console.log(evt) }}>
-                <NavDropdown.Item eventKey="map1">Action</NavDropdown.Item>
-                <NavDropdown.Item eventKey="map2">Another action</NavDropdown.Item>
-                <NavDropdown.Item eventKey="map3">Something</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="address..." className="mr-sm-2" />
-              <Button variant="outline-success">Am I in a flood plain?</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
-        {this.whichmap === "map1" && <Map1></Map1>}
-      </div>
-    );
+    if (this.state.mode === 'view') {
+      return (
+        <div>
+          <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+            <Navbar.Brand href="#home">Flood Zone</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Resources" id="basic-nav-dropdown">
+                  <NavDropdown.Item onClick={this.handleEdit}>Action</NavDropdown.Item>
+                  <NavDropdown.Item onClick={this.handleSave}>Another action</NavDropdown.Item>
+                  <NavDropdown.Item onClick={this.handleEdit}>Something</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form inline>
+                <FormControl type="text" placeholder="address..." className="mr-sm-2" />
+                <Button variant="outline-success">Am I in a flood plain?</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Navbar>
+          <Map1 ></Map1>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div>
+          <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+            <Navbar.Brand href="#home">Flood Zone</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Resources" id="basic-nav-dropdown" onSelect={function (evt) { console.log(evt) }}>
+                  <NavDropdown.Item onClick={this.handleEdit}>Action</NavDropdown.Item>
+                  <NavDropdown.Item onClick={this.handleSave}>Another action</NavDropdown.Item>
+                  <NavDropdown.Item onClick={this.handleEdit}>Something</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form inline>
+                <FormControl type="text" placeholder="address..." className="mr-sm-2" />
+                <Button variant="outline-success">Am I in a flood plain?</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Navbar>
+
+        </div>
+      );
+    }
   }
 
 }
