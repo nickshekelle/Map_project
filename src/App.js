02a528ea53f0a1a61c8157e3e0fb7213d7ed4971
Nick Shekelle <nshekelle@varmour.com>
2019-08-08 10:48:50 -0700

    conditional rendering working

--- src/App.js
+++ src/App.js
@@ -15,28 +15,27 @@ import Button from 'react-bootstrap/Button'
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { mode: 'view' };
+    this.state = { view: 'map1' };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSave = this.handleSave.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
+    this.change_to_map1 = this.change_to_map1.bind(this);
+    this.change_to_map2 = this.change_to_map2.bind(this);
+    this.change_to_map3 = this.change_to_map3.bind(this);
   }
 
-  handleChange(e) {
-    this.setState({ inputText: e.target.value });
+  change_to_map1() {
+    this.setState({ view: 'map1' });
   }
 
-  handleSave() {
-    this.setState({ mode: 'view' });
+  change_to_map2() {
+    this.setState({ view: 'map2' });
   }
 
-  handleEdit() {
-    this.setState({ mode: 'edit' });
+  change_to_map3() {
+    this.setState({ view: 'map3' });
   }
-
 
   render() {
-    if (this.state.mode === 'view') {
+    if (this.state.view === 'map1') {
       return (
         <div>
           <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
@@ -47,9 +46,9 @@ export default class App extends Component {
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#link">Link</Nav.Link>
                 <NavDropdown title="Resources" id="basic-nav-dropdown">
-                  <NavDropdown.Item onClick={this.handleEdit}>Action</NavDropdown.Item>
-                  <NavDropdown.Item onClick={this.handleSave}>Another action</NavDropdown.Item>
-                  <NavDropdown.Item onClick={this.handleEdit}>Something</NavDropdown.Item>
+                  <NavDropdown.Item onClick={this.change_to_map1}>Map1</NavDropdown.Item>
+                  <NavDropdown.Item onClick={this.change_to_map2}>Map2</NavDropdown.Item>
+                  <NavDropdown.Item onClick={this.change_to_map3}>Map3</NavDropdown.Item>
                 </NavDropdown>
               </Nav>
               <Form inline>
@@ -62,7 +61,7 @@ export default class App extends Component {
         </div>
       );
     }
-    else {
+    else if (this.state.view === "map2") {
       return (
         <div>
           <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
@@ -73,9 +72,9 @@ export default class App extends Component {
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#link">Link</Nav.Link>
                 <NavDropdown title="Resources" id="basic-nav-dropdown" onSelect={function (evt) { console.log(evt) }}>
-                  <NavDropdown.Item onClick={this.handleEdit}>Action</NavDropdown.Item>
-                  <NavDropdown.Item onClick={this.handleSave}>Another action</NavDropdown.Item>
-                  <NavDropdown.Item onClick={this.handleEdit}>Something</NavDropdown.Item>
+                  <NavDropdown.Item onClick={this.change_to_map1}>Map1</NavDropdown.Item>
+                  <NavDropdown.Item onClick={this.change_to_map2}>Map2</NavDropdown.Item>
+                  <NavDropdown.Item onClick={this.change_to_map3}>Map3</NavDropdown.Item>
                 </NavDropdown>
               </Nav>
               <Form inline>
@@ -84,7 +83,30 @@ export default class App extends Component {
               </Form>
             </Navbar.Collapse>
           </Navbar>
-
+        </div>
+      );
+    } else if (this.state.view === "map3") {
+      return (
+        <div>
+          <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+            <Navbar.Brand href="#home">Flood Zone</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Resources" id="basic-nav-dropdown" onSelect={function (evt) { console.log(evt) }}>
+                  <NavDropdown.Item onClick={this.change_to_map1}>Map1</NavDropdown.Item>
+                  <NavDropdown.Item onClick={this.change_to_map2}>Map2</NavDropdown.Item>
+                  <NavDropdown.Item onClick={this.change_to_map3}>Map3</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form inline>
+                <FormControl type="text" placeholder="address..." className="mr-sm-2" />
+                <Button variant="outline-success">Am I in a flood plain?</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Navbar>
         </div>
       );
     }
@@ -94,5 +116,4 @@ export default class App extends Component {
 
 export function renderToDOM(container) {
   render(<div />, container);
-  render(<Map1 />, container);
 }
